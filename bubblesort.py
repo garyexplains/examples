ed1b4c77141c44bf5b787d3eaf9b9d789ab0be08
Gary Explains <40464104+garyexplains@users.noreply.github.com>
2018-06-21 15:41:30 +0300

    Created bubblesort.py
    Understanding Bubble Sort with Examples and Code!
    https://youtu.be/WOjc48sVo6E

---
+++ bubblesort.py
@@ -0,0 +1,27 @@
+#
+# Understanding Bubble Sort with Examples and Code!
+# https://youtu.be/WOjc48sVo6E
+#
+def bubblesort(list):
+        swapped = True
+        while swapped:
+                print
+                print "New iteration..."
+                swapped = False
+                for i in range(len(list)-1):
+                        if(list[i] > list[i+1]):
+                                print "Index: " + str(i) + " - Swap " + str(list
+[i]) + " with " + str(list[i+1])
+                                tmp = list [i]
+                                list[i] = list[i+1]
+                                list[i+1] = tmp
+                                swapped = True
+                                print list
+        print "Nothing left to swap. Done"
+        return list
+
+l = [5,8,1,4,2]
+print l
+
+l = bubblesort(l)
+print l
