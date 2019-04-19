d403fadcac639649946e19bdb7501b19d483ec76
Gary Explains <40464104+garyexplains@users.noreply.github.com>
2019-04-19 12:43:07 +0300

    Write HELLO
    Assumes 0xFBFF is output address

---
+++ example asm/hello.asm
@@ -0,0 +1,20 @@
+LD R1, 0xFBFF
+# H
+LD R2, 72
+STL R2, R1
+# e
+ADD R1, 1
+LD R2, 101
+STL R2, R1
+# l
+ADD R1, 1
+LD R2, 108
+STL R2, R1
+# l
+ADD R1, 1
+LD R2, 108
+STL R2, R1
+# o
+ADD R1, 1
+LD R2, 111
+STL R2, R1
