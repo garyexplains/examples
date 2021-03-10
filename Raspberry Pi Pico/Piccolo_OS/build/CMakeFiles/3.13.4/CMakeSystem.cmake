dacc3c595eb35a245deadb788ccd893858ab5e27
Gary Explains <40464104+garyexplains@users.noreply.github.com>
2021-03-10 15:06:39 +0200

    Add files via upload

---
+++ Raspberry Pi Pico/Piccolo_OS/build/CMakeFiles/3.13.4/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-5.10.11-v7l+")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "5.10.11-v7l+")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "armv7l")
+
+include("/home/pi/pico/pico-sdk/cmake/preload/toolchains/pico_arm_gcc.cmake")
+
+set(CMAKE_SYSTEM "PICO")
+set(CMAKE_SYSTEM_NAME "PICO")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "cortex-m0plus")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
