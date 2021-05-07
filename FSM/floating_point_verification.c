c7edc3d42154634da22a8c4cd5a164cd915045c5
Gary Explains <40464104+garyexplains@users.noreply.github.com>
2021-05-07 20:12:02 +0300

    Update floating_point_verification.c

--- FSM/floating_point_verification.c
+++ FSM/floating_point_verification.c
@@ -8,32 +8,32 @@
 #include <string.h>
 
 typedef enum {
-    START,
-    AFTER_MINUS,
-    AFTER_DOT,
-    __VALID_END_STATES,
-    SECOND_DIGIT_ONWARDS,
-    MANTISSA,
-    __LAST_STATE
-} FSM_STATES_t;
+  START,
+  AFTER_MINUS,
+  AFTER_DOT,
+  internal_VALID_END_STATES,
+  SECOND_DIGIT_ONWARDS,
+  MANTISSA,
+  internal_LAST_STATE
+} FSM_STATES;
 
 typedef struct {
-  FSM_STATES_t state;
+  FSM_STATES state;
   char *tokens;
-  FSM_STATES_t next_state;
-} FSM_STATE_INFO_t;
+  FSM_STATES next_state;
+} FSM_STATE_INFO;
 
-int run(FSM_STATES_t start_state, FSM_STATE_INFO_t *state_machine, char *cargo) {
+int run(FSM_STATES start_state, FSM_STATE_INFO *state_machine, char *cargo) {
 
-  FSM_STATE_INFO_t *p;
-  FSM_STATES_t current_state = start_state;
-  FSM_STATE_INFO_t *found;
+  FSM_STATE_INFO *p;
+  FSM_STATES current_state = start_state;
+  FSM_STATE_INFO *found;
   char *c = cargo;
 
   while (*c != 0) {
     found = NULL;
     p = state_machine;
-    while (p->state != __LAST_STATE) {
+    while (p->state != internal_LAST_STATE) {
       if (p->state == current_state) {
         if (strchr(p->tokens, *c) != NULL) {
           found = p;
@@ -49,35 +49,32 @@ int run(FSM_STATES_t start_state, FSM_STATE_INFO_t *state_machine, char *cargo) 
       printf("%s is bad. Failed at %c\n", cargo, *c);
       return -1;
     }
-    }
-    if(current_state > __VALID_END_STATES) {
-      printf("%s is good.\n", cargo);
-      return 1;
-    } else {
-      printf("%s is bad. More needed.\n", cargo);
-      return -2;
-    }
+  }
+  if (current_state > internal_VALID_END_STATES) {
+    printf("%s is good.\n", cargo);
+    return 1;
+  } else {
+    printf("%s is bad. More needed.\n", cargo);
+    return -2;
+  }
 }
 
-int main(int argc, char *argv[])
-{
-    static FSM_STATE_INFO_t state_machine[] = {
-        {START, "1234567890", SECOND_DIGIT_ONWARDS},
-        {START, "-", AFTER_MINUS},
-        {AFTER_MINUS, "1234567890", SECOND_DIGIT_ONWARDS},
-        {SECOND_DIGIT_ONWARDS, "1234567890", SECOND_DIGIT_ONWARDS},
-        {SECOND_DIGIT_ONWARDS, ".", AFTER_DOT},
-        {AFTER_DOT, "1234567890", MANTISSA},
-        {MANTISSA, "1234567890", MANTISSA},
-        {__LAST_STATE, "", __LAST_STATE}
-    };
+int main(int argc, char *argv[]) {
+  static FSM_STATE_INFO state_machine[] = {
+      {START, "1234567890", SECOND_DIGIT_ONWARDS},
+      {START, "-", AFTER_MINUS},
+      {AFTER_MINUS, "1234567890", SECOND_DIGIT_ONWARDS},
+      {SECOND_DIGIT_ONWARDS, "1234567890", SECOND_DIGIT_ONWARDS},
+      {SECOND_DIGIT_ONWARDS, ".", AFTER_DOT},
+      {AFTER_DOT, "1234567890", MANTISSA},
+      {MANTISSA, "1234567890", MANTISSA},
+      {internal_LAST_STATE, "", internal_LAST_STATE}};
 
-    run(START, state_machine, "3.14");
-    run(START, state_machine, "7.77");
-    run(START, state_machine, "-7");
-    run(START, state_machine, "-22.0");
-    run(START, state_machine, "--22.0");
-    run(START, state_machine, "-22.a0");
-    run(START, state_machine, "-1.");
-    run(START, state_machine, "-");
+  run(START, state_machine, "3.14");
+  run(START, state_machine, "-7");
+  run(START, state_machine, "-22.0");
+  run(START, state_machine, "--22.0");
+  run(START, state_machine, "-22.a0");
+  run(START, state_machine, "-1.");
+  run(START, state_machine, "-");
 }
